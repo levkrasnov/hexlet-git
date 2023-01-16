64059077c55143cc76ad77628ff642befe959892
Lev Krasnov <leevakrasnov@yandex.ru>
2023-01-16 16:31:19 +0500

    f

---
+++ lesson_tests/capitalize.py
@@ -0,0 +1,11 @@
+def capitalize(text):
+    if text == '':
+        return ''
+    first_char = text[0].upper()
+    rest_substring = text[:1]
+    return f'{first_char}{rest_substring}'
+
+
+
+if __name__ == '__main__':
+    main()
